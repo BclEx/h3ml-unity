5f64df831b47a7ff289ae06585e62e5de4269bb8
Sky Morey <smorey@degdigital.com>
2018-03-05 18:08:46 -0600

    updates

---
+++ src.object/ObjectManager/Assets/Scripts/MakeBox.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakeBox : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+
+        var chair = Resources.Load("Hatchet");
+
+        var box = Instantiate(GameObject.Find("Cube00"), this.transform);
+        box.transform.position += new Vector3(2, 2, 2);
+        Debug.Log("Test");
+        //GameObject.Destroy(box);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
