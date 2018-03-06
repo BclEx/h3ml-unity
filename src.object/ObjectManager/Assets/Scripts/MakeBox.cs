e54c83f424eb6afa122b30c961b3bbed0b231db0
Sky Morey <smorey@degdigital.com>
2018-03-06 08:15:32 -0600

    updates

--- src.object/ObjectManager/Assets/Scripts/MakeBox.cs
+++ src.object/ObjectManager/Assets/Scripts/MakeBox.cs
@@ -8,11 +8,11 @@ public class MakeBox : MonoBehaviour
     void Start()
     {
 
-        var chair = Resources.Load("Hatchet");
+        var chair = ResourcesEx.LoadFromUri("https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png");
 
-        var box = Instantiate(GameObject.Find("Cube00"), this.transform);
-        box.transform.position += new Vector3(2, 2, 2);
-        Debug.Log("Test");
+        //var box = Instantiate(GameObject.Find("Cube00"), this.transform);
+        //box.transform.position += new Vector3(2, 2, 2);
+        //Debug.Log("Test");
         //GameObject.Destroy(box);
     }
 
